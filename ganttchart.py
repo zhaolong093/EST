a8b053dd4a15ffe4268e33530accbbdb907ba1cc
zhaolong093 <63531209+zhaolong093@users.noreply.github.com>
2025-11-04 15:57:24 +1100

    EST_ASG3_Updated_GanttChart

--- ganttchart.py
+++ ganttchart.py
@@ -12,21 +12,26 @@ if os.path.exists(csv_path):
     df = pd.read_csv(csv_path)
 else:
     df = pd.DataFrame([
-        {"Task": "Register (F1)",                                           "Start": "2025-11-03", "End": "2025-11-14"},
-        {"Task": "Login (F2)",                                              "Start": "2025-11-10", "End": "2025-11-25"},
-        {"Task": "Compare (F3)",                                            "Start": "2025-11-17", "End": "2025-12-05"},
-        {"Task": "Search Box (F4)",                                         "Start": "2025-12-05", "End": "2025-12-20"},
-        {"Task": "Product Cart (F5)",                                       "Start": "2025-12-10", "End": "2026-01-08"},
-        {"Task": "Swiper Controller buttons (F6)",                          "Start": "2026-01-01", "End": "2026-01-15"},
-        {"Task": "Top collection category filter (F7)",                     "Start": "2026-01-15", "End": "2026-01-20"},
-        {"Task": "Top collection Product Detail (F8)",                      "Start": "2026-01-18", "End": "2026-01-23"},
-        {"Task": "Edit Account Information (F9)",                           "Start": "2026-01-20", "End": "2026-01-25"},
-        {"Task": "Make a comment on blog (F10)",                            "Start": "2025-11-25", "End": "2025-12-10"},
-        {"Task": "Filtering search result (F11)",                           "Start": "2026-01-05", "End": "2026-01-15"},
-        {"Task": "Write a product review (F12)",                            "Start": "2025-12-10", "End": "2025-12-30"},
-        {"Task": "Add address in address book (F13)",                       "Start": "2026-01-08", "End": "2026-01-12"},
-        {"Task": "Purchase a Gift Certificate (F14)",                       "Start": "2026-01-20", "End": "2026-01-25"},
-        {"Task": "Top collection category filter (F15)",                    "Start": "2026-01-15", "End": "2026-01-30"},
+
+        #fix all of these
+        {"Task": "Register (F1)",                                           "Start": "2025-11-03", "End": "2025-11-10"},
+        {"Task": "Login (F2)",                                              "Start": "2025-11-10", "End": "2025-11-20"},
+        {"Task": "Compare (F3)",                                            "Start": "2026-01-25", "End": "2026-01-30"},
+        {"Task": "Search Box (F4)",                                         "Start": "2025-12-10", "End": "2025-12-25"},
+        {"Task": "Product Cart (F5)",                                       "Start": "2025-12-15", "End": "2025-12-30"},
+        {"Task": "Update Product Quantity  (F6)",                                       "Start": "2026-01-01", "End": "2026-01-10"},
+        {"Task": "Swiper Controller buttons (F7)",                          "Start": "2026-01-9", "End": "2026-01-15"},
+        {"Task": "Top collection category filter (F8)",                     "Start": "2026-01-15", "End": "2026-01-20"},
+        {"Task": "Top collection Product Detail (F9)",                      "Start": "2026-01-18", "End": "2026-01-23"},
+        {"Task": "Edit Account Information (F10)",                           "Start": "2025-11-20", "End": "2025-11-30"},
+        {"Task": "Make a comment on blog (F11)",                            "Start": "2025-11-30", "End": "2025-12-15"},
+        {"Task": "Filtering Product (F12)",                           "Start": "2026-01-15", "End": "2026-01-25"},
+        {"Task": "Write a product review (F13)",                            "Start": "2025-12-25", "End": "2026-01-05"},
+        {"Task": "Add address in address book (F14)",                       "Start": "2025-12-01", "End": "2025-12-15"},
+        {"Task": "Purchase a Gift Certificate (F15)",                       "Start": "2026-01-20", "End": "2026-01-25"},
+        {"Task": "Quick View Modal  (F16)",                    "Start": "2026-01-15", "End": "2026-01-20"},
+        {"Task": "Add to Wish List (F17)",                    "Start": "2026-01-20", "End": "2026-01-25"},
+        {"Task": "Product Comparison (F18)",                    "Start": "2025-11-10", "End": "2025-11-20"},
     ])
 
 # 2) Prep data ---------------------------------------------------------------
@@ -51,7 +56,7 @@ ax.xaxis.set_major_formatter(mdates.DateFormatter("%d %b %Y"))
 fig.autofmt_xdate()
 
 ax.set_xlabel("Timeline")
-ax.set_title("Project Gantt Chart")
+ax.set_title("Software Testing Gantt Chart")
 plt.tight_layout()
 
 #save pic as png
